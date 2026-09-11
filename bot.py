7a79b28414932bf467c9547336a4329258386b6f
project-alpha-lab-cmd <mauricioreyescultura@gmail.com>
2026-09-10 23:35:48 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,7 +92,7 @@ def main():
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     print("KINIKBot iniciado correctamente...")
-    app.run_polling()
+        app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
